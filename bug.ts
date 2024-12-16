6c9b84c5ff5521006e8b10483f57ac3b153cd303
BugHunter-X <askzenai@gmail.com>
2024-12-16 19:16:07 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+let user = ["Jane", "Doe"];
+
+console.log(greeter(user)); //This will throw an error because it expects a single string
